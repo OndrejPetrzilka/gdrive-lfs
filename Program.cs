d4b70b3aba5b9aef1312d50755c284af94649d10
ondrej.petrzilka <ondrej.petrzilka@gmail.com>
2024-04-30 08:46:31 +0200

    * initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GoogleDriveLFS
+{
+    internal class Program
+    {
+        static readonly string FileName = "c:\\Data\\Projects\\drive-test3\\log.txt";
+
+        static void Main(string[] args)
+        {
+            File.WriteAllLines(FileName, args);
+            File.AppendAllText(FileName, "\r\n\r\n");
+            while (true)
+            {
+                string? str = Console.ReadLine();
+                if (str == null)
+                    return;
+
+                File.AppendAllLines(FileName, new string[] { str });
+            }
+        }
+    }
+}
