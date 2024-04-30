362cbdf5a0282721c2fa6e0ba30745ae8ce44356
Ondrej Petrzilka <ondrej.petrzilka@gmail.com>
2024-04-30 18:44:40 +0200

    + preparing global setup

---
+++ install.bat
@@ -0,0 +1,3 @@
+git config --global lfs.customtransfer.gdrivelfs.path "%~dp0GoogleDriveLFS.exe"
+git config --global lfs.customtransfer.gdrivelfs.args 
+git config --global lfs."https://drive.google.com".standalonetransferagent gdrivelfs
