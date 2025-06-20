1275e160d7af4d852e321debee0e1bde6f210019
Ondrej Petrzilka <ondrej.petrzilka@gmail.com>
2025-06-20 06:47:39 +0200

    * SetupGit.bat updated

--- SetupGit.bat
+++ SetupGit.bat
@@ -1,3 +1,11 @@
-git config --global lfs.customtransfer.gdrivelfs.path "%~dp0GoogleDriveLFS.exe"
-git config --global lfs.customtransfer.gdrivelfs.args 
-git config --global lfs."https://drive.google.com".standalonetransferagent gdrivelfs
+@echo off
+git config --global lfs.customtransfer.gdrivelfs_extra.path ".lfstool/GoogleDriveLFS.exe"
+git config --global lfs."https://drive-extra.google.com".standalonetransferagent gdrivelfs_extra
+
+IF  '%ERRORLEVEL%'=='0' (
+  echo GoogleDriveLFS setup successful
+) ELSE (
+  echo Error occured while setting up GoogleDriveLFS: %ERRORLEVEL%
+)
+
+pause
